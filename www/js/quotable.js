80c2ddec6ac5f9ba33c94602facf5860fd445b68
sspagnuolo <sergio@voltdata.info>
2018-09-19 16:17:46 -0300

    muda posicao de logo e selo
    muda bold da aspa

--- www/js/quotable.js
+++ www/js/quotable.js
@@ -83,7 +83,7 @@ function processText() {
 
 function saveImage() {
     // first check if the quote actually fits
-    if (($source.offset().top + $source.height()) > $logoWrapper.offset().top) {
+    if (($source.offset().top + $source.height()) < $logoWrapper.offset().top) {
         alert("Your quote doesn't quite fit. Shorten the text or choose a smaller font-size.");
         return;
     }
